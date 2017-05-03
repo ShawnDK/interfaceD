2e2539e9058feaea58b716f243f9c071055bb752
Rot <thaodo0492@yahoo.com>
2017-05-03 14:36:34 +0200

    hcdh

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+	header('Access-Control-Allow-Origin: *');	
+	header('Content-Type: application/json');
+	$email = $_POST["email"];
+	$password = $_POST["password"];
+
+	$sAdmins = file_get_contents("admins.json");
+	$ajAdmins = json_decode( $sAdmins );
+	$success = false;
+	for ($i=0; $i < count($ajAdmins); $i++) { 
+		if ($ajAdmins[$i]->email == $email && $ajAdmins[$i]->password == $password) {
+			$success = true;
+			break;
+		}
+	}
+
+echo json_encode(["success"=> $success]);
+?>
