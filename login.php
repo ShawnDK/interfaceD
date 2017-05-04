909df696a9222f6c49b0ae35d1b57be2185060e1
Rot <thaodo0492@yahoo.com>
2017-05-05 00:07:55 +0200

    aaa

--- login.php
+++ login.php
@@ -8,12 +8,17 @@
 	$sAdmins = file_get_contents("admins.json");
 	$ajAdmins = json_decode( $sAdmins );
 	$success = false;
+
 	for ($i=0; $i < count($ajAdmins); $i++) { 
 		if ($ajAdmins[$i]->email == $email && $ajAdmins[$i]->password == $password) {
+            //echo $ajAdmins[$i]->email;
 			$success = true;
 			break;
-		}
+            
+            }
+        
 	}
 
 echo json_encode(["success"=> $success]);
+
 ?>
